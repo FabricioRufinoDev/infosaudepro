a4d7c04613b9e4cbf42be387d93c471bf6680d8a
Yago-stack <fabricioruf@gmail.com>
2025-11-11 08:35:38 -0300

    .
    .

--- src/main/java/com/infosaudepro/exemplo/dto/PacienteCadastroDTO.java
+++ src/main/java/com/infosaudepro/exemplo/dto/PacienteCadastroDTO.java
@@ -6,4 +6,28 @@ public class PacienteCadastroDTO {
     private String nome;
     private String cpf; // Em texto puro antes de ser criptografado
     private String diagnostico; // Em texto puro antes de ser criptografado
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
 }
